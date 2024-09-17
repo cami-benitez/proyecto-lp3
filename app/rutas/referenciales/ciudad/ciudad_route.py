e4e75e87a3872cfe7e4b7da762a3fb466ed34f4d
cami benitez <camila.benitez0021@gmail.com>
2024-09-17 14:13:30 -0400

    fix: limpieza de modulo

--- app/rutas/referenciales/ciudad/ciudad_route.py
+++ app/rutas/referenciales/ciudad/ciudad_route.py
@@ -1,18 +1,7 @@
-from flask import Blueprint, render_template, jsonify
-from app.dao.referenciales.ciudad.CiudadDao import CiudadDao 
+from flask import Blueprint, render_template
 
 ciumod = Blueprint('ciudad', __name__, template_folder='templates')
 
 @ciumod.route('/ciudad-index')
 def ciudadIndex():
-     ciudao = CiudadDao()
-     return render_template('ciudad-index.html', lista_ciudades=ciudao.getCiudades())
-
-@ciumod.route('/ciudad-agregar')
-def ciudadAgregar():
-     return render_template('ciudad-agregar.html')
-# REST
-@ciumod.route('/get-Ciudades')
-def getCiudades():
-     ciudao = CiudadDao()
-     return jsonify(ciudao.getCiudades())
+    return render_template('ciudad-index.html')
