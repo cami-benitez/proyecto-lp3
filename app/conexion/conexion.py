9b950f9bdfaa046a1bb7db4e5b5e1c6d58711e90
cami benitez <camila.benitez0021@gmail.com>
2024-09-26 18:12:00 -0400

    feat: ultimos cambios

--- app/conexion/conexion.py
+++ app/conexion/conexion.py
@@ -5,7 +5,7 @@ class Conexion:
     def __init__(self):
         self.con = psycopg2.connect(dbname="lp3-bd", user="postgres", host="localhost", password="6740362")
         """getConexion 
-        self
+        
         retorno la instalacion de la base de datos 
         """
     def getConexion(self):
