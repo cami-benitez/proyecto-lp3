30df2f0fd8458b7c3bdafe4e356a8b1ecbd67fae
cami benitez <camila.benitez0021@gmail.com>
2024-10-02 11:16:29 -0400

    fead: referencial de persona y ajustes en pais

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 #impotar referenciales 
 from app.rutas.referenciales.ciudad.ciudad_route import ciumod 
 from app.rutas.referenciales.pais.pais_route import paimod
+from app.rutas.referenciales.persona.persona_route import permod
 
 # registrar referenciales 
 modulo0 ='/referenciales'
@@ -19,9 +20,17 @@ app.register_blueprint(paimod, url_prefix=f'{modulo0}/pais')
 
 from app.rutas.referenciales.pais.pais_api import paiapi
 
+modulo0 = '/referenciales'
+app.register_blueprint(permod, url_prefix=f'{modulo0}/persona')
+
+from app.rutas.referenciales.persona.persona_api import perapi
+
 # APIS v1
 version1 = '/api/v1'
 app.register_blueprint(ciuapi, url_prefix=version1)
 
 version1 = '/api/v1'
 app.register_blueprint(paiapi, url_prefix=version1)
+
+version1 = '/api/v1'
+app.register_blueprint(perapi, url_prefix=version1)
--- referenciales.sql
+++ referenciales.sql
@@ -3,3 +3,11 @@ CREATE TABLE
         id serial PRIMARY KEY 
         , descripcion varchar(60) UNIQUE
     );
+
+CREATE TABLE
+	personas(
+		id serial PRIMARY KEY,
+		descripcion varchar(60) UNIQUE,
+		apellido varchar(50),
+		numero_cedula decimal(8) UNIQUE
+	);
